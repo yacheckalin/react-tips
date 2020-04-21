816338a72b1fa370abcc440fc197a3ce8d9babcd
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2020-04-21 14:58:10 +0300

    fixed import path in App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import M from "materialize-css/dist/js/materialize.min";
 import SearchForm from "./react-patterns/controlled-component/SearchForm";
 import Clicker from "./react-patterns/state-hoisting/Clicker";
 import NavigationLayout from "./react-patterns/layout-component/NavigationLayout";
-import UserContainer from "./react-patterns/сontainer-component/UserContainer";
+import UserContainer from "./react-patterns/container-component/UserContainer";
 
 const App = () => (
   <div>
