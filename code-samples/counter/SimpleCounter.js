edfd3d45f0463b0ac6b3c3a540c773ac1aede5f7
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2020-05-02 10:27:37 +0300

    added e2e tests for counter

--- code-samples/counter/SimpleCounter.js
+++ code-samples/counter/SimpleCounter.js
@@ -8,13 +8,23 @@ const SimpleCounter = ({ initial }) => {
     <div className="container">
       <div className="row">
         <div className="col s1">
-          <span className="flow-text" onClick={(e) => setCounter(counter - 1)}>
+          <span
+            id="desc_counter"
+            className="flow-text"
+            onClick={(e) => setCounter(counter - 1)}
+          >
             <i className="material-icons red-text">remove_circle_outline</i>
           </span>
         </div>
-        <div className="col s2 blue-text">{counter}</div>
+        <div className="col s2 blue-text" id="counter_value">
+          {counter}
+        </div>
         <div className="col s1">
-          <span className="flow-text" onClick={(e) => setCounter(counter + 1)}>
+          <span
+            id="asc_counter"
+            className="flow-text"
+            onClick={(e) => setCounter(counter + 1)}
+          >
             <i className="material-icons green-text">add_circle</i>
           </span>
         </div>
--- code-samples/counter/SimpleCounter.test.js
+++ code-samples/counter/SimpleCounter.test.js
@@ -9,4 +9,27 @@ describe("<SimpleCounter />", () => {
 
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
+
+  test("initial value shows properly", () => {
+    const wrapper = shallow(<SimpleCounter initial={0} />);
+
+    expect(wrapper.find("#counter_value").text()).toBe("0");
+    wrapper.unmount();
+  });
+
+  test("ascending properly", () => {
+    const wrapper = shallow(<SimpleCounter initial={0} />);
+
+    wrapper.find("#asc_counter").simulate("click");
+    expect(wrapper.find("#counter_value").text()).toBe("1");
+    wrapper.unmount();
+  });
+
+  test("descending properly", () => {
+    const wrapper = shallow(<SimpleCounter initial={10} />);
+
+    wrapper.find("#desc_counter").simulate("click");
+    expect(wrapper.find("#counter_value").text()).toBe("9");
+    wrapper.unmount();
+  });
 });
